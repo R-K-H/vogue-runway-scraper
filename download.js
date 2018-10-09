0274c6bd209ed8f3ef39f5bfedc3fd921f84752f
Kollan House <kollanh@gmail.com>
2018-10-09 16:58:23 +0100

    adjusted for graphql query to get the actual images for a show.

--- download.js
+++ download.js
@@ -6,68 +6,90 @@ const https = require('https')
 const fs = require('fs')
 const _ = require('lodash')
 const request = require('request')
-//const download = require('image-downloader')
 
 const show = 'spring-2019-ready-to-wear'
 // Bring in all files for scraping
 const imageSets = require('./image-urls.json')
+
+const newImageSets = require('./show-images.json')
 
 const download = function(options){
 	return new Promise(function(resolve, reject) {
 		var uri = options.uri
 		var dir = options.dir
 		var filename = options.filename
+		let fileSizeInBytes = 0
 		if (!fs.existsSync(dir)){
 		    fs.mkdirSync(dir);
 		}
+		if (fs.existsSync(dir + filename)) {
+			let stats = fs.statSync(dir + filename)
+			fileSizeInBytes = stats.size
+		}
+
 		request.get(uri)
 		.on('error', function(err) {
 		    console.log(err)
 		    reject(err)
 		})
 		.on('response', function(response) {
-		    console.log(response.statusCode) // 200
-		    console.log(response.headers['content-type']) // 'image/png'
-		})
-		.pipe(fs.createWriteStream(dir + filename))
-		.on('close', function() {
+		    if(fileSizeInBytes == response.headers['content-length']) {
+		    	console.log(filename)
+		    	console.log('content-length: ' + response.headers['content-length'] + '/' + fileSizeInBytes)
 		    	resolve(true)
+		    } else {
+		    	console.log('Downloading ' + filename + ' with a size of ' + (response.headers['content-length'] / 1000000) + 'MB' )
+		    	response.pipe(fs.createWriteStream(dir + filename))
+				.on('close', function() {
+				    resolve(true)
+				})
+		    }
 		})
 	})
+}
+
+const getImages2 = () => {
+	var p = Promise.resolve();
+	_.forEach(newImageSets, async function(set) {
+		var i = 0;
+		_.forEach(_.values(set)[0], async function(image) {
+				i ++
+				var url = image
+
+				var designer = _.keys(set)[0].toUpperCase().replace(/(-+)/g, ' ')
+				// Download to a directory and save with the original filename
+				const options = {
+				  uri: url,
+				  dir: __dirname + '/images/' + show + '/' + designer + '/',
+				  filename:  designer + ' ' + i + '.jpg'         // Save to /path/to/dest/image.jpg
+				}
+				p = p.then(download.bind(null, options));
+		})
+	})
+	p.then(_ => console.log('done'));
 }
 
 // Iterate through each script object in the file
-const getImages = function() {
-	_.forEach(imageSets, function(set) {
+const getImages = async function() {
+	var p = Promise.resolve();
+	_.forEach(imageSets, async function(set) {
 		var i = 0;
 		_.forEach(_.values(set)[0], async function(image) {
-			if(i > 4){
-				return
-			}
-			i ++
-			var url = image.replace('\\"url\\":\\"', '')
-			url = url.replace('\\"','')
+				i ++
+				var url = image.replace('\\"url\\":\\"', '')
+				url = url.replace('\\"','')
 
-			var designer = _.keys(set)[0].toUpperCase().replace(/(\/+)/g, ' ')
-			designer = designer.replace(/(\s+)/g, '\$1')
-			// Download to a directory and save with the original filename
-			const options = {
-			  uri: url,
-			  dir: __dirname + '/images/' + show + '/' + designer + '/',
-			  filename:  designer + ' ' + i + '.jpg'         // Save to /path/to/dest/image.jpg
-			}
-			await download(options)
-			.then(resp => {console.log(resp)})
-			.catch(err => console.log(err))
-			// download.image(options)
-			//   .then(({ filename, image }) => {
-			//     console.log('File saved to', filename)
-			//   })
-			//   .catch((err) => {
-			//     console.error(err)
-			//   })
+				var designer = _.keys(set)[0].toUpperCase().replace(/(\/+)/g, ' ')
+				designer = designer.replace(/(\s+)/g, '\$1')
+				// Download to a directory and save with the original filename
+				const options = {
+				  uri: url,
+				  dir: __dirname + '/images/' + show + '/' + designer + '/',
+				  filename:  designer + ' ' + i + '.jpg'         // Save to /path/to/dest/image.jpg
+				}
+				p = p.then(download.bind(null, options));
 		})
-		
 	})
+	p.then(_ => console.log('done'));
 }
-getImages()
+getImages2()
