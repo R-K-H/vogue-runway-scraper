578ad2f1b77833c05b29a87933f281758a871d74
Kollan House <kollanh@gmail.com>
2019-03-05 20:19:35 -0800

    revised the application to use class for functions. added in promises to as not hammer API requests. added in other various functions to handle passing data along as well as storing into files so we can keep going even if the application fails.

--- future.js
+++ future.js
@@ -1,98 +1,60 @@
+// This fetches from the page the total number of looks in the show for the season
+    // TODO: We should really use this count to enforce the number of images we want to retrieve
+    // from the graphql.
+    getShowLookCount(collections) {
+        _.forEach(collections, function(collection) {
+            let collectionTitle = collection.title
+            let content = "";  
+            let req = https.get(collection.link, function(res) {
+            res.setEncoding("utf8");
+            res.on("data", function (chunk) {
+                content += chunk;
+            });
 
-function DiffObjects(o1, o2) {
-    // choose a map() impl.
-    // you may use $.map from jQuery if you wish
-    var map = Array.prototype.map?
-        function(a) { return Array.prototype.map.apply(a, Array.prototype.slice.call(arguments, 1)); } :
-        function(a, f) { 
-            var ret = new Array(a.length), value;
-            for ( var i = 0, length = a.length; i < length; i++ ) 
-                ret[i] = f(a[i], i);
-            return ret.concat();
-        };
+            res.on("end", function () {
+            x(content, '.gallery-marker--count@html | countToInt')(function(err, obj){
+                        let collect = {}
+                        collect[collectionTitle] = obj
+                        fs.appendFile('look-counts.json', JSON.stringify(collect) + ',\n', function (err) {
+                          if (err) throw err;
+                          console.log('Saved!');
+                        });
+                    })
+            });
+            });
 
-    // shorthand for push impl.
-    var push = Array.prototype.push;
-
-    // check for null/undefined values
-    if ((o1 == null) || (o2 == null)) {
-        if (o1 != o2)
-            return [["", "null", o1!=null, o2!=null]];
-
-        return undefined; // both null
-    }
-    // compare types
-    if ((o1.constructor != o2.constructor) ||
-        (typeof o1 != typeof o2)) {
-        return [["", "type", Object.prototype.toString.call(o1), Object.prototype.toString.call(o2) ]]; // different type
-
-    }
-
-    // compare arrays
-    if (Object.prototype.toString.call(o1) == "[object Array]") {
-        if (o1.length != o2.length) { 
-            return [["", "length", o1.length, o2.length]]; // different length
-        }
-        var diff =[];
-        for (var i=0; i<o1.length; i++) {
-            // per element nested diff
-            var innerDiff = DiffObjects(o1[i], o2[i]);
-            if (innerDiff) { // o1[i] != o2[i]
-                // merge diff array into parent's while including parent object name ([i])
-                push.apply(diff, map(innerDiff, function(o, j) { o[0]="[" + i + "]" + o[0]; return o; }));
-            }
-        }
-        // if any differences were found, return them
-        if (diff.length)
-            return diff;
-        // return nothing if arrays equal
-        return undefined;
+            req.end();
+        })
     }
 
-    // compare object trees
-    if (Object.prototype.toString.call(o1) == "[object Object]") {
-        var diff =[];
-        // check all props in o1
-        for (var prop in o1) {
-            // the double check in o1 is because in V8 objects remember keys set to undefined 
-            if ((typeof o2[prop] == "undefined") && (typeof o1[prop] != "undefined")) {
-                // prop exists in o1 but not in o2
-                diff.push(["[" + prop + "]", "undefined", o1[prop], undefined]); // prop exists in o1 but not in o2
-
-            }
-            else {
-                // per element nested diff
-                var innerDiff = DiffObjects(o1[prop], o2[prop]);
-                if (innerDiff) { // o1[prop] != o2[prop]
-                    // merge diff array into parent's while including parent object name ([prop])
-                    push.apply(diff, map(innerDiff, function(o, j) { o[0]="[" + prop + "]" + o[0]; return o; }));
-                }
-
-            }
-        }
-        for (var prop in o2) {
-            // the double check in o2 is because in V8 objects remember keys set to undefined 
-            if ((typeof o1[prop] == "undefined") && (typeof o2[prop] != "undefined")) {
-                // prop exists in o2 but not in o1
-                diff.push(["[" + prop + "]", "undefined", undefined, o2[prop]]); // prop exists in o2 but not in o1
-
-            }
-        }
-        // if any differences were found, return them
-        if (diff.length)
-            return diff;
-        // return nothing if objects equal
-        return undefined;
+    // Simple function to count the number of images we have in each folder
+    // to compare with the look count to make sure we didn't do anything wrong.
+    // TODO: See TODO
+    getShowFolderImageCount(source) {
+        let directories = fs.readdirSync(source).map(name => join(source, name)).filter(isDirectory)
+        let results = []
+        _.forEach(directories, (directory) => {
+            let obj = {}
+            fs.readdir(directory, (err, files) => {
+                let name = directory.replace(source + '/', '')
+                obj[name] = files.length
+                let cleanObj = JSON.stringify(obj)
+                // TODO: Format this in an array for us to compare to the look count
+                console.log(cleanObj)
+            })
+        })
     }
-    // if same type and not null or objects or arrays
-    // perform primitive value comparison
-    if (o1 != o2)
-        return [["", "value", o1, o2]];
 
-    // return nothing if values are equal
-    return undefined;
-}
-
+    loadFiles() {
+        let show
+        // Bring in the files
+        const files = ['./json/shows.json',
+                                        './json/look-counts.json',
+                                        './json/folder-counts.json',
+                                        './json/images.json'
+                                    ]
+        shows = JSON.parse(fs.readFileSync('./json/shows.json', 'utf8'));
+    }
 
 const compareFolderImageCountToLookCount = (folderCount, lookCount) => {
 	folderCount.sort(function(a,b){
---
+++ vogue.js
@@ -0,0 +1,256 @@
+const https = require('https')
+const fs = require('fs')
+const _ = require('lodash')
+const { join } = require('path')
+const Xray = require('x-ray')
+const request = require('request')
+const prependFile = require('prepend-file')
+
+const x = Xray({filters: {
+    encodeUri: (value) => {
+		return typeof value === 'string' ? encodeURIComponent(value) : value
+    },
+    stripSlug: (value) => {
+    	var regex = /(?=[^\/]+$)(.*)/
+    	return typeof value === 'string' ? regex.exec(value)[0] : value
+    },
+    formatSlug: (value) => {
+    	return typeof value === 'string' ? value.toUpperCase().replace(/(-+)/g, ' ') : value
+    },
+    countToInt: (value) => {
+    	return typeof value === 'string' ? parseInt(value) : value
+    },
+    nullify: (value) => { 
+    	return value.length ? value : null
+    }
+}})
+
+const isDirectory = source => fs.lstatSync(source).isDirectory()
+
+class Vogue {
+	constructor(show){
+		this.show = show
+		// Settings for app scraping
+		this.imageDirectory = __dirname + '/images/' + this.show // TODO: Cleanup not sure this needs to be here.
+		this.jsonDirectory = __dirname + '/json/'
+		this.showImages = {}
+	}
+
+	checkFile(file) {
+		return new Promise((resolve, reject) => {
+			try {
+			  if (fs.existsSync(file)) {
+			    resolve(JSON.parse(fs.readFileSync(file, 'utf8')))
+			  } else {
+			  	console.log('Empty file found.')
+			  	resolve({})
+			  }
+			} catch(err) {
+			  reject(err)
+			}
+		})
+	}
+
+	async checkStep(file, stepCount) {
+		let readFile = await this.checkFile(file)
+		if(_.isEmpty(readFile)){
+			if(stepCount === 1) {
+				let looks = await this.getShowsLooksList()
+				if(looks) {
+					fs.appendFile('./json/' + this.show + '-looks.json', JSON.stringify(looks), function (err) {
+					  if (err) {
+					  	console.log(err)
+					  }
+					  console.log('saved all looks to json file')
+					})
+					return new Promise((resolve, reject) => { resolve(looks) })
+				}
+			} else if (stepCount === 2){
+				let images = await this.getAllShowsLooksImages()
+				console.log(images)
+				if(images) {
+					fs.appendFile('./json/' + this.show + '-images.json', JSON.stringify(this.showImages), function (err) {
+					  if (err) {
+					  	console.log(err)
+					  }
+					  console.log('saved all images to json file')
+					})
+					return new Promise((resolve, reject) => { resolve(images) })
+				}
+			}
+		} else {
+			return new Promise((resolve, reject) => { resolve(readFile) })
+		}
+	}
+
+	// Build list of all the shows with their vogue url as well as their titles
+	getShowsLooksList() {
+		return new Promise((resolve, reject) => {
+			x('https://www.vogue.com/fashion-shows/' + this.show, 
+				'.season-module li .tab-list--item', [
+					{
+						prettyTitle: 'a@html | encodeUri',
+						title: 'a@href | stripSlug | formatSlug',
+						link: 'a@href',
+						slug: 'a@href | stripSlug'
+					}
+				]
+			)
+				.then((res) => {resolve(res)})
+		})
+	}
+
+	// This gets all the looks for each show by iterating though the getShowImagesUrl which writes it to a JSON file
+	async getAllShowsLooksImages() {
+		let file = './json/' + this.show + '-looks.json'
+		// Check to see if file has contents if not run to generate
+		let collections = await this.checkStep(file, 1)
+		if(collections) {
+			return new Promise((resolve, reject) => {
+				let slugs = []
+				_.forEach(collections, (collection) => {
+					if(collection.prettyTitle !== null){
+						slugs.push(collection.slug)
+					}
+				})
+				let sortedSlugs = slugs.sort()
+				let p = Promise.resolve()
+				let promises = []
+				_.forEach(sortedSlugs, (slug, index, array) => {
+					let params = {
+						slug: slug, 
+						count: 200,
+						last: false,
+						index: index,
+						length: array.length
+					}
+					promises.push(p = p.then(this.getShowImagesUrl.bind(this, params)))
+				})
+				Promise.all(promises).then(resp => {
+					if(resp) {
+						 resolve(this.showImages)
+					}
+				})
+			})
+		}
+	}
+
+	// This is what gets the images from the JSON file, it creates an array of
+	// promises in order to ensure each image is downloaded before the next one starts
+	// so as not to a) kill your internet connection (show for 2019-spring was ~20GB)
+	// and b) not to upset vogue with too many requests.
+	async downloadLookImages() {
+		let file = './json/' + this.show + '-images.json'
+		// Check to see if file has contents if not run to generate
+		let imageSets = await this.checkStep(file, 2)
+		if(imageSets) {
+			let p = Promise.resolve()
+			_.forEach(imageSets, (set) => {
+				let i = 0
+				_.forEach(set, (image) => {
+					i ++
+					var url = image
+					var designer = _.keys(imageSets)[i-1].toUpperCase().replace(/(-+)/g, ' ')
+					// Download to a directory and save with the original filename
+					const params = {
+					  uri: url,
+					  dir: __dirname + '/images/' + this.show + '/' + designer + '/',
+					  filename:  designer + ' ' + i + '.jpg'         // Save to /path/to/dest/image.jpg
+					}
+					p = p.then(this.downloadImage.bind(this, params))
+				})
+			})
+			p.then(resp => {
+				if(resp) {
+					console.log('done downloading all images')
+					return new Promise((resolve, reject) => { resolve(true) })
+				}
+			})
+		}
+	}
+
+	// This uses vogues graphql to get a list from the shows using the slug and a modified count (to fetch them all)
+	// TODO: Look into their actual graphql to see if we could simplify this and retrieve what we want to in a more ordered fashion vs
+	// their user for slideshows.
+	getShowImagesUrl(params) {
+		return new Promise((resolve, reject) => {
+			let query = '/graphql?query=query%20SlideshowPortraitPageRelayPaginationQuery($brandSlug:String!%20$count:Int!%20$cursor:String%20$galleryType:FashionGalleryTypeEnum!%20$hierarchy:String%20$seasonSlug:String!%20$slug:String){...SlideshowPortraitPageRelay}fragment%20SlideshowPortraitPageRelay%20on%20Root{...SiteContainerRelay%20...EndSlideRelay_data%20...SwipeSlideshowRelay_data%20fashionShow:fashionShowV2(slug:$slug){...RunwaySlideshowHeaderRelay_fashionShow%20photosTout{__typename%20...%20on%20Image{url%20id}...%20on%20Video{id}...%20on%20Clip{id}}...SlideshowTitleRelay_fashionShow%20...EndSlideRelay_fashionShow%20...SwipeSlideshowRelay_fashionShow%20...SocialSharerRelay%20...BaseHelmetRelay%20url%20brand{slug%20id}season{slug%20id}galleries{collection{id}atmosphere{id}beauty{id}detail{id}frontRow{id}}id}fashionGallery:fashionGalleryByType(brandSlug:$brandSlug,seasonSlug:$seasonSlug,galleryType:$galleryType){...SlideshowAdColumnRelay_fashionGallery%20...SocialSharerRelay%20slideCount%20slidesV2(after:$cursor,first:$count){edges{node{__typename%20...%20on%20SlideInterface{...GridViewRelay_slides%20caption%20credit%20id%20photosTout{__typename%20...%20on%20Image{url%20id}...%20on%20Video{id}...%20on%20Clip{id}}}...%20on%20CollectionSlide{details{...%20on%20Slide{photosTout{__typename%20...%20on%20Image{url%20id}...%20on%20Video{id}...%20on%20Clip{id}}}id}modaLink%20moveItVideo%20id}...%20on%20Slide{id}}cursor}pageInfo{endCursor%20hasNextPage}}id}}fragment%20SiteContainerRelay%20on%20Root{...FeedHeaderRelay%20...SiteHeaderRelay}fragment%20EndSlideRelay_data%20on%20Root{allContent(first:10,type:[%22FashionShowV2%22]){edges{node{__typename%20...%20on%20FashionShowV2{id%20url%20brand{name%20id}season{name%20id}galleries{atmosphere{photosTout{__typename%20...%20on%20Image{altText%20url%20id}...%20on%20Video{id}...%20on%20Clip{id}}id}beauty{photosTout{__typename%20...%20on%20Image{altText%20url%20id}...%20on%20Video{id}...%20on%20Clip{id}}id}collection{photosTout{__typename%20...%20on%20Image{altText%20url%20id}...%20on%20Video{id}...%20on%20Clip{id}}id}detail{photosTout{__typename%20...%20on%20Image{altText%20url%20id}...%20on%20Video{id}...%20on%20Clip{id}}id}frontRow{photosTout{__typename%20...%20on%20Image{altText%20url%20id}...%20on%20Video{id}...%20on%20Clip{id}}id}}}id}}}}fragment%20SwipeSlideshowRelay_data%20on%20Root{...EndSlideRelay_data}fragment%20RunwaySlideshowHeaderRelay_fashionShow%20on%20FashionShowV2{...SlideshowTitleRelay_fashionShow}fragment%20SlideshowTitleRelay_fashionShow%20on%20FashionShowV2{brand{name%20url%20id}season{name%20url%20id}}fragment%20EndSlideRelay_fashionShow%20on%20FashionShowV2{...SlideshowTitleRelay_fashionShow%20...SocialSharerRelay%20season{name%20url%20id}}fragment%20SwipeSlideshowRelay_fashionShow%20on%20FashionShowV2{...EndSlideRelay_fashionShow%20brand{slug%20id}}fragment%20SocialSharerRelay%20on%20Content{__typename%20id%20title%20url%20photosTout{__typename%20...%20on%20Image{url%20id}...%20on%20MixedMedia{default{__typename%20...%20on%20Image{title%20url}id}}...%20on%20Video{id}...%20on%20Clip{id}}GMTPubDate%20GMTModDate%20meta{facebook{title%20description%20imageURL}seo{title%20description%20keywords}twitter{title%20description%20imageURL}}}fragment%20BaseHelmetRelay%20on%20Content{...%20on%20FashionShowV2{reviewContributors:review{contributor{artist{name%20id}author{name%20id}editor{name%20id}photographer{name%20id}videographer{name%20id}}id}}GMTPubDate%20GMTModDate%20channels{id%20name}channel{id%20name%20parent{id%20name}}tags{id%20name}contributor{artist{name%20id}author{name%20id}editor{name%20id}photographer{name%20id}videographer{name%20id}}meta{facebook{title%20description%20imageURL}seo{title%20description%20keywords}twitter{title%20description%20imageURL}}}fragment%20SlideshowAdColumnRelay_fashionGallery%20on%20FashionShowGallery{...SocialSharerRelay}fragment%20GridViewRelay_slides%20on%20SlideInterface{photosTout{__typename%20...%20on%20Image{url%20id}...%20on%20Video{id}...%20on%20Clip{id}}}fragment%20FeedHeaderRelay%20on%20Root{headerTerm:term(hierarchy:$hierarchy){...TermSocialSharerRelay%20name%20description%20pageHeader{colorScheme%20bannerLink%20bannerLinkTitle%20bannerImage}parent{name%20pageHeader{colorScheme%20bannerLink%20bannerLinkTitle%20bannerImage}id}id}}fragment%20SiteHeaderRelay%20on%20Root{allSeasons(first:10){edges{node{id%20name%20url}}}}fragment%20TermSocialSharerRelay%20on%20Term{name%20url%20meta{seo{title%20description%20keywords}}}&variables='
+			
+			let vars = '{%22brandSlug%22:%22' + params.slug + '%22,%22count%22:'+ params.count +',%22galleryType%22:%22collection%22,%22hierarchy%22:null,%22seasonSlug%22:%22' + this.show + '%22,%22slug%22:null}'
+			let fullQuery = query + vars
+			var options = {
+			  host: 'graphql.vogue.com',
+			  path: fullQuery
+			}
+			let content = ""
+			let req = https.request(options, (res) => {
+		    res.setEncoding("utf8")
+		    res.on("data", (chunk) => {
+		        content += chunk
+		    })
+
+		    res.on("end", () => {
+		    	let obj = JSON.parse(content)
+		    	if(obj.data.hasOwnProperty('fashionGallery') && obj.data.fashionGallery != null) {
+			    	let images = obj.data.fashionGallery.slidesV2.edges
+			    	let imageArry = []
+			    	_.forEach(images, (image) => {
+			    		imageArry.push(image.node.photosTout.url)
+			    	})
+			    	this.showImages[params.slug] = imageArry
+			    	console.log('fetching images list for ' + params.slug + ' ' + (params.index + 1) + '/' + params.length)
+			    	resolve(true)
+					} else {
+						reject(false)
+					}
+		    })
+			})
+			// Was going to make this just output then build into a entire string output to file
+			req.end()
+		})
+	}
+
+	// This makes a request to fetch the images it does a check to see if the folder already exists
+	// and if it does if the image exists and it's of the same size, if not it creates the folder, and 
+	// saves the image.
+	downloadImage(params) {
+		return new Promise((resolve, reject) => {
+			const uri = params.uri
+			const dir = params.dir
+			const filename = params.filename
+			let fileSizeInBytes = 0
+			if (!fs.existsSync(dir)){
+			  fs.mkdirSync(dir)
+			}
+			if (fs.existsSync(dir + filename)) {
+				console.log('File already exists.')
+				let stats = fs.statSync(dir + filename)
+				fileSizeInBytes = stats.size
+			}
+
+			request.get(uri)
+				.on('error', (err) => {
+			    console.log(err)
+			    reject(err)
+				})
+				.on('response', (response) => {
+			    if(fileSizeInBytes == response.headers['content-length']) {
+			    	console.log(filename)
+			    	console.log('content-length: ' + response.headers['content-length'] + '/' + fileSizeInBytes)
+			    	resolve(true)
+			    } else {
+			    	console.log('Downloading ' + filename + ' with a size of ' + (response.headers['content-length'] / 1048576) + 'MB' )
+			    	response.pipe(fs.createWriteStream(dir + filename))
+					.on('close', () => {
+					    resolve(true)
+					})
+			    }
+				})
+		})
+	}
+
+	async run(){
+		await this.downloadLookImages()
+	}
+}
+module.exports = Vogue
