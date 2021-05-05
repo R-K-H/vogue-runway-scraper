e2d4e679598d277c6b53997b1cbc860fc271e639
Kollan House <kollanh@gmail.com>
2021-05-05 15:53:45 -0400

    feat: adds input for use on machine, adjusts to scrape designers various tweaks to ensure data

--- app.js
+++ app.js
@@ -1,12 +1,62 @@
+const prompts = require('prompts')
+const Vogue = require('./vogue.js')
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-const Vogue = require('./vogue.js')
+try {
+	const show = process.env.SHOW // Replace if no env
+	const rateLimit = process.env.RATE_LIMIT // Replace if no env
+	const designer = process.env.DESIGNER
+} catch (error) {
+	console.log(error)
+}
 
-const show = process.env.SHOW // Replace if no env
-const rateLimit = process.env.RATE_LIMIT // Replace if no env
 
-const vogue = new Vogue(show, rateLimit)
 
-vogue.run()
+(async () => {
+	const questions = [
+		{
+			type: 'text',
+	    	name: 'designerBool',
+	    	message: 'Are you interested in scraping a designer?',
+		},
+		{
+			type: prev => prev.toLowerCase() == 'yes' ? 'text' : null,
+			name: 'designer',
+			message: 'Which designer?'
+		},
+		{
+			type: prev => prev == 'no' ? 'text' : null,
+	    	name: 'show',
+	    	message: 'Which show would you like to scrape?',
+		},
+		
+		{
+			type: 'number',
+	    	name: 'rateLimit',
+	    	message: 'What would you like your rate limit to be? (in milliseconds)',
+		},
+	]
+	const onSubmit = (prompt, answer) => console.log(`Thanks I got ${answer} from ${prompt.name}`);
+	const response = await prompts(questions, { onSubmit });
+	let _designer = null
+	let _show = null
+	console.log(response)
+	if (response.designerBool.toLowerCase() != 'no' && response.hasOwnProperty('designer')) {
+		_designer = response.designer
+		console.log('designer triggered')
+	} else {
+		_show = response.show
+		console.log('show triggered')
+	}
+	let _rateLimit = response.rateLimit
+	const vogue = new Vogue(_rateLimit, _show, _designer)
+	vogue.run()
+})();
+
+
+
+
+
--- vogue.js
+++ vogue.js
@@ -10,7 +10,7 @@ const x = Xray({ filters: {
     return typeof value === 'string' ? encodeURIComponent(value) : value
   },
   stripSlug: value => {
-    const regex = /(?=[^/]+$)(.*)/
+    const regex = /(?=[^\/]+$)(.*)/
     return typeof value === 'string' ? regex.exec(value)[0] : value
   },
   formatSlug: value => {
@@ -21,22 +21,33 @@ const x = Xray({ filters: {
   },
   nullify: value => {
     return value.length ? value : null
+  },
+  makeSmall: value => {
+    return typeof value === 'string' ? value.toLowerCase().replace(/ /g, '-') : value
   }
 } })
 
 class Vogue {
-  constructor (show, rateLimit) {
+  constructor (rateLimit, show = null, designer = null) {
     this.show = show
+    if (show != null) {
+      this.show = show.toLowerCase()
+    }
     this.rateLimit = rateLimit
     // Master object
     this.showImages = {}
+    this.designer
+    if (designer != null) {
+      this.designer = designer.toLowerCase()
+    }
   }
+
   async checkFile (file) {
     try {
       if (fs.existsSync(file)) {
         return await JSON.parse(fs.readFileSync(file, 'utf8'))
       } else {
-        console.log('Empty file found.')
+        console.log('Empty file found for: ', file)
         return {}
       }
     } catch (err) {
@@ -46,29 +57,50 @@ class Vogue {
 
   checkStep = async (file, stepCount) => {
     let readFile = await this.checkFile(file)
+    let fileName = this.show
+    if (this.designer != null) {
+      fileName = this.designer
+    }
     if (_.isEmpty(readFile)) {
       if (stepCount === 1) {
-        let looks = await this.getShowsLooksList()
-        console.log(looks)
-        if (looks) {
-          fs.appendFile('./json/' + this.show + '-looks.json', JSON.stringify(looks), err => {
-            if (err) {
-              console.log(err)
-            }
-            console.log('Saved all looks to json file')
-          })
-          return new Promise((resolve, reject) => { resolve(looks) })
-        }
+        if (this.designer != null) {
+          let shows = await this.getDesignerShowsList()
+          console.log('shows:', shows)
+          if (shows) {
+            fs.appendFile('./json/' + fileName + '-shows.json', JSON.stringify(shows), err => {
+              if (err) {
+                console.log('error with looks file', err)
+              }
+              console.log('Saved all looks to json file')
+            })
+            return new Promise((resolve, reject) => { resolve(shows) })
+          }
+        } else {
+          let looks = await this.getShowsLooksList()
+          console.log('looks:', looks)
+          if (looks) {
+            fs.appendFile('./json/' + fileName + '-looks.json', JSON.stringify(looks), err => {
+              if (err) {
+                console.log('error with looks file', err)
+              }
+              console.log('Saved all looks to json file')
+            })
+            return new Promise((resolve, reject) => { resolve(looks) })
+          }
+         }
       } else if (stepCount === 2) {
-        let images = await this.getAllShowsLooksImages()
-        if (images) {
-          fs.appendFile('./json/' + this.show + '-images.json', JSON.stringify(this.showImages), err => {
+        await this.getAllShowsLooksImages()
+        console.log('returned we awaited images')
+        if (this.showImages) {
+          fs.appendFile('./json/' + fileName + '-images.json', JSON.stringify(this.showImages), err => {
             if (err) {
-              console.log(err)
+              console.log('error with images file', err)
             }
             console.log('Saved all images to json file')
           })
-          return new Promise((resolve, reject) => { resolve(images) })
+          return new Promise((resolve, reject) => { resolve(this.showImages) })
+        } else {
+          console.log("Unable to fetch images, something went wrong.")
         }
       }
     } else {
@@ -76,59 +108,107 @@ class Vogue {
     }
   }
 
+  getDesignerShowsList() {
+    if (this.designer != null) {
+      return new Promise((resolve, reject) => {
+        x('https://www.vogue.com/fashion-shows/designer/' + this.designer,
+          '.grid.grid-even a', [
+            {
+              show: 'h3@html | makeSmall'
+            }
+          ]
+        ).then(res => { resolve(res) })
+      })
+    } else {
+      console.log("You didn't specify a designer.")
+    }
+  }
+
   // Build list of all the shows with their vogue url as well as their titles
   getShowsLooksList () {
     return new Promise((resolve, reject) => {
-      x('https://www.vogue.com/fashion-shows/' + this.show,
+      x('https://www.vogue.com/fashion-shows/' + this.show, 
         '.grouped-navigation--a-to-z li', [
           {
             prettyTitle: 'a@html | encodeUri',
             title: 'a@href | stripSlug | formatSlug',
             link: 'a@href',
-            slug: 'a@href | stripSlug'
+            brandSlug: 'a@href | stripSlug'
           }
         ]
-      )
-        .then(res => { resolve(res) })
+      ).then(res => { resolve(res) })
     })
   }
 
   // This gets all the looks for each show by iterating though the getShowImagesUrl which writes it to a JSON file
   getAllShowsLooksImages = async () => {
-    let file = './json/' + this.show + '-looks.json'
+    let fileName = this.show + '-looks.json'
+    if (this.designer != null) {
+      fileName = this.designer + '-shows.json'
+    }
+    let file = './json/' + fileName
     // Check to see if file has contents if not run to generate
     let collections = await this.checkStep(file, 1)
     if (collections) {
-      let slugs = []
-      for (let i = 0; collections.length > i; i++) {
-        if (collections[i].prettyTitle !== null) {
-          slugs.push(collections[i].slug)
-        }
-      }
-      let sortedSlugs = slugs.sort()
       let p = Promise.resolve()
       let promises = []
-      for (let i = 0; sortedSlugs.length > i; i++) {
-        let params = {
-          slug: sortedSlugs[i].slug,
-          count: 200,
-          last: false,
-          index: i,
-          length: sortedSlugs.length
+      if (this.designer != null) {
+        const brandShows = collections
+        let shows = []
+        for (let i = 0; brandShows.length > i; i++) {
+          shows.push(brandShows[i].show)
         }
-        promises.push(p = p.then(this.getShowImagesUrl.bind(this, params))
-          .catch(err => console.log('\x1b[41m%s\x1b[0m', err))
-        )
+        let sortedShows = shows.sort()
+        for (let i = 0; sortedShows.length > i; i++) {
+          let params = {
+            brandSlug: this.designer,
+            count: 200,
+            last: false,
+            index: i,
+            length: sortedShows.length,
+            show: sortedShows[i]
+          }
+          await promises.push(p = p.then(this.getShowImagesUrl.bind(this, params))
+            .catch(err => console.log('\x1b[41m%s\x1b[0m', err))
+          )
+        }
+      } else {
+        let _show = this.show
+        let brandSlug = []
+        for (let i = 0; collections.length > i; i++) {
+          if (collections[i].hasOwnProperty('prettyTitle') && collections[i].prettyTitle !== null) {
+            if (collections[i].brandSlug !== undefined && !collections[i].brandSlug.toLowerCase().includes("#")) {
+              brandSlug.push(collections[i].brandSlug)
+            }
+          }
+        }
+        
+        
+        let sortedBrandSlug = brandSlug.sort()
+        
+        for (let i = 0; sortedBrandSlug.length > i; i++) {
+          let params = {
+            brandSlug: sortedBrandSlug[i],
+            count: 200,
+            last: false,
+            index: i,
+            length: sortedBrandSlug.length,
+            show: _show
+          }
+          await promises.push(p = p.then(this.getShowImagesUrl.bind(this, params))
+            .catch(err => console.log('\x1b[41m%s\x1b[0m', err))
+          )
+        }
       }
-      Promise.all(promises)
+      await Promise.all(promises)
         .then(resp => {
           if (resp) {
-            return this.showImages
+            return new Promise((resolve, reject) => { resolve(this.showImages) })
           }
         })
         .catch(error => {
-          console.log(error)
-          return false
+          console.log("error", error)
+          return new Promise((resolve, reject) => { reject(false) })
         })
     }
   }
@@ -138,7 +218,11 @@ class Vogue {
   // so as not to a) kill your internet connection (show for 2019-spring was ~20GB)
   // and b) not to upset vogue with too many requests.
   downloadLookImages = async () => {
-    let file = './json/' + this.show + '-images.json'
+    let fileName = this.show
+    if (this.designer != null) {
+      fileName = this.designer
+    }
+    let file = './json/' + fileName + '-images.json'
     // Check to see if file has contents if not run to generate
     let imageSets = await this.checkStep(file, 2)
     if (imageSets) {
@@ -149,12 +233,20 @@ class Vogue {
         _.forEach(set, image => {
           i++
           let url = image
-          let designer = _.keys(imageSets)[j].toUpperCase().replace(/(-+)/g, ' ')
+          let designer = this.designer
+          let show = this.show
+          if (this.designer != null) {
+            show = _.keys(imageSets)[j]
+          } else {
+            designer = _.keys(imageSets)[j]
+          }
+          show = show.toLowerCase()
+          designer = designer.toUpperCase().replace(/(-+)/g, ' ')
           // Download to a directory and save with the original filename
           const params = {
             uri: url,
-            parentDir: path.join(__dirname, '/images/', this.show),
-            dir: path.join(__dirname, '/images/', this.show, '/', designer, '/'),
+            parentDir: path.join(__dirname, '/images/', show),
+            dir: path.join(__dirname, '/images/', show, '/', designer, '/'),
             designer: designer,
             filename: designer + ' ' + i + '.jpg' // Save to /path/to/dest/image.jpg
           }
@@ -176,10 +268,87 @@ class Vogue {
   // ordered fashion vs their user for slideshows.
   getShowImagesUrl (params) {
     return new Promise((resolve, reject) => {
-      let query = '/graphql?query=query%20SlideshowPortraitPageRelayPaginationQuery($brandSlug:String!%20$count:Int!%20$cursor:String%20$galleryType:FashionGalleryTypeEnum!%20$hierarchy:String%20$seasonSlug:String!%20$slug:String){...SlideshowPortraitPageRelay}fragment%20SlideshowPortraitPageRelay%20on%20Root{...SiteContainerRelay%20...EndSlideRelay_data%20...SwipeSlideshowRelay_data%20fashionShow:fashionShowV2(slug:$slug){...RunwaySlideshowHeaderRelay_fashionShow%20photosTout{__typename%20...%20on%20Image{url%20id}...%20on%20Video{id}...%20on%20Clip{id}}...SlideshowTitleRelay_fashionShow%20...EndSlideRelay_fashionShow%20...SwipeSlideshowRelay_fashionShow%20...SocialSharerRelay%20...BaseHelmetRelay%20url%20brand{slug%20id}season{slug%20id}galleries{collection{id}atmosphere{id}beauty{id}detail{id}frontRow{id}}functionalTags{name%20id}id}fashionGallery:fashionGalleryByType(brandSlug:$brandSlug,seasonSlug:$seasonSlug,galleryType:$galleryType){...SlideshowAdColumnRelay_fashionGallery%20...SocialSharerRelay%20slideCount%20slidesV2(after:$cursor,first:$count){edges{node{__typename%20...%20on%20SlideInterface{...GridViewRelay_slides%20caption%20credit%20id%20photosTout{__typename%20...%20on%20Image{url%20id}...%20on%20Video{id}...%20on%20Clip{id}}}...%20on%20CollectionSlide{details{...%20on%20Slide{photosTout{__typename%20...%20on%20Image{url%20id}...%20on%20Video{id}...%20on%20Clip{id}}}id}id}...%20on%20Slide{id}}cursor}pageInfo{endCursor%20hasNextPage}}id}}fragment%20SiteContainerRelay%20on%20Root{...FeedHeaderRelay%20...SiteHeaderRelay}fragment%20EndSlideRelay_data%20on%20Root{allContent(first:10,type:[%22FashionShowV2%22]){edges{node{__typename%20...%20on%20FashionShowV2{id%20url%20brand{name%20id}season{name%20id}galleries{atmosphere{photosTout{__typename%20...%20on%20Image{altText%20url%20id}...%20on%20Video{id}...%20on%20Clip{id}}id}beauty{photosTout{__typename%20...%20on%20Image{altText%20url%20id}...%20on%20Video{id}...%20on%20Clip{id}}id}collection{photosTout{__typename%20...%20on%20Image{altText%20url%20id}...%20on%20Video{id}...%20on%20Clip{id}}id}detail{photosTout{__typename%20...%20on%20Image{altText%20url%20id}...%20on%20Video{id}...%20on%20Clip{id}}id}frontRow{photosTout{__typename%20...%20on%20Image{altText%20url%20id}...%20on%20Video{id}...%20on%20Clip{id}}id}}}id}}}}fragment%20SwipeSlideshowRelay_data%20on%20Root{...EndSlideRelay_data}fragment%20RunwaySlideshowHeaderRelay_fashionShow%20on%20FashionShowV2{...SlideshowTitleRelay_fashionShow}fragment%20SlideshowTitleRelay_fashionShow%20on%20FashionShowV2{brand{name%20url%20id}season{name%20url%20id}}fragment%20EndSlideRelay_fashionShow%20on%20FashionShowV2{...SlideshowTitleRelay_fashionShow%20...SocialSharerRelay%20season{name%20url%20id}}fragment%20SwipeSlideshowRelay_fashionShow%20on%20FashionShowV2{...EndSlideRelay_fashionShow%20brand{slug%20id}}fragment%20SocialSharerRelay%20on%20Content{__typename%20id%20title%20url%20photosTout{__typename%20...%20on%20Image{url%20id}...%20on%20MixedMedia{default{__typename%20...%20on%20Image{title%20url}id}}...%20on%20Video{id}...%20on%20Clip{id}}GMTPubDate%20GMTModDate%20meta{facebook{title%20description%20imageURL}seo{title%20description%20keywords}twitter{title%20description%20imageURL}}}fragment%20BaseHelmetRelay%20on%20Content{...%20on%20FashionShowV2{reviewContributors:review{contributor{artist{name%20id}author{name%20id}editor{name%20id}photographer{name%20id}videographer{name%20id}}id}functionalTags{name%20id}}GMTPubDate%20GMTModDate%20channels{id%20name}channel{id%20name%20parent{id%20name}}tags{id%20name}contributor{artist{name%20id}author{name%20id}editor{name%20id}photographer{name%20id}videographer{name%20id}}meta{facebook{title%20description%20imageURL}seo{title%20description%20keywords}twitter{title%20description%20imageURL}}}fragment%20SlideshowAdColumnRelay_fashionGallery%20on%20FashionShowGallery{...SocialSharerRelay}fragment%20GridViewRelay_slides%20on%20SlideInterface{photosTout{__typename%20...%20on%20Image{url%20id}...%20on%20Video{id}...%20on%20Clip{id}}}fragment%20FeedHeaderRelay%20on%20Root{headerTerm:term(hierarchy:$hierarchy){...TermSocialSharerRelay%20name%20description%20pageHeader{colorScheme%20bannerLink%20bannerLinkTitle%20bannerImage}parent{name%20pageHeader{colorScheme%20bannerLink%20bannerLinkTitle%20bannerImage}id}id}}fragment%20SiteHeaderRelay%20on%20Root{allSeasons(first:10){edges{node{id%20name%20url}}}}fragment%20TermSocialSharerRelay%20on%20Term{name%20url%20meta{facebook{title%20description%20imageURL}seo{title%20description%20keywords}twitter{title%20description%20imageURL}}}&variables='
+      let query = `
+            /graphql?query=query%20SlideshowPortraitPageRelayPaginationQuery(
+              $brandSlug:String!%20
+              $count:Int!%20
+              $cursor:String%20
+              $galleryType:FashionGalleryTypeEnum!%20
+              $seasonSlug:String!%20
+            ){...SlideshowPortraitPageRelay}
+            fragment%20
+            SlideshowPortraitPageRelay%20
+            on%20
+            Root{
+              fashionShow:fashionShowV2(slug:null){photosTout{
+                   __typename%20
+                   ...%20
+                   on%20
+                   Image{url%20id}
+                   ...%20
+                   on%20
+                   Video{id}
+                   ...%20
+                   on%20
+                   Clip{id}
+                 }
+                 url%20
+                 brand{slug%20id}
+                 season{slug%20id}
+                 galleries{
+                   collection{id}
+                   atmosphere{id}
+                   beauty{id}
+                   detail{id}
+                   frontRow{id}
+                 }
+                 functionalTags{name%20id}
+                id
+               }
+               fashionGallery:fashionGalleryByType(
+                 brandSlug:$brandSlug,
+                 seasonSlug:$seasonSlug,
+                 galleryType:$galleryType
+               ){
+                 slideCount%20
+                 slidesV2(
+                   after:$cursor,
+                   first:$count
+                 ){
+                   edges{
+                     node{
+                         __typename%20
+                         ...%20
+                         on%20
+                         SlideInterface{
+                           id%20
+                           photosTout{
+                             __typename%20
+                             ...%20
+                             on%20
+                             Image{url%20id}
+                           }
+                         }
+                         ...%20
+                         on%20
+                         Slide{id}
+                       }
+                       cursor
+                     }
+                   }
+                 }
+               }
+      `
 
-      let vars = '{%22brandSlug%22:%22' + params.slug + '%22,%22count%22:'+ params.count +',%22galleryType%22:%22collection%22,%22hierarchy%22:null,%22seasonSlug%22:%22' + this.show + '%22,%22slug%22:null}'
+      let vars = `&variables={
+                              %22brandSlug%22:%22${params.brandSlug}%22,
+                              %22count%22:${params.count},
+                              %22galleryType%22:%22collection%22,
+                              %22seasonSlug%22:%22${params.show}%22
+                            }`
       let fullQuery = query + vars
+      fullQuery = fullQuery.replace(/\s/g, "");
+      // console.log(fullQuery)
       let options = {
         host: 'graphql.vogue.com',
         path: fullQuery
@@ -200,15 +369,20 @@ class Vogue {
               _.forEach(images, image => {
                 imageArry.push(image.node.photosTout.url)
               })
-              this.showImages[params.slug] = imageArry
-              console.log('\x1B[0GFetching images list for ' + params.slug + ' ' + (params.index + 1) + '/' + params.length + '\x1B[0G')
+              if (this.designer != null) {
+                this.showImages[params.show] = imageArry
+              } else {
+                this.showImages[params.brandSlug] = imageArry
+              }
+              
+              console.log('\x1B[0GFetching images list for ' + params.brandSlug + ' ' + (params.index + 1) + '/' + params.length + '\x1B[0G')
               // TODO: Write to file as we go along.
               setTimeout(resolve, this.rateLimit)
             } else {
-              reject('no images found for ' + params.slug + ' ' + (params.index + 1) + '/' + params.length)
+              reject('no images found for ' + params.brandSlug + ' ' + (params.index + 1) + '/' + params.length)
             }
           } else {
-            reject('fetch failed for ' + params.slug + ' ' + (params.index + 1) + '/' + params.length)
+            reject('fetch failed for ' + params.brandSlug + ' ' + (params.index + 1) + '/' + params.length)
           }
         })
       })
@@ -242,7 +416,7 @@ class Vogue {
 
       request.get(uri)
         .on('error', err => {
-          console.log(err)
+          console.log('error', err)
           reject(err)
         })
         .on('response', response => {
