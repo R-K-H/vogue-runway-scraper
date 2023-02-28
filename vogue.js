bc455ae1a4fc2de2aac0dbfb1abd332269c8bb99
Kollan House <kollanh@gmail.com>
2023-02-28 15:48:56 -0500

    fix: adds header data for failed 403

--- vogue.js
+++ vogue.js
@@ -31,7 +31,13 @@ class Vogue {
       let fullUrl = host + params
       fullUrl = fullUrl.replace(/\s/g, "%20");
       let options = {
-        method: 'GET'
+        method: 'GET',
+        headers: {
+          "Content-Type": "application/json",
+          "Host": "graphql.vogue.com",
+          "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+        },
+        referrerPolicy: "origin",
       }
       const response = await fetch(fullUrl, options)
       const json = await response.json()
